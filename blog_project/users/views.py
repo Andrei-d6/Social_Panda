bd5d7c9dbd1108b388ad80501470d712a7cfe147
Andrei-d6 <andrei.dugaesescu@gmail.com>
2020-05-21 08:59:59 +0300

    Resolved bugs added by black

--- blog_project/users/views.py
+++ blog_project/users/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-
+from .models import Profile
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
